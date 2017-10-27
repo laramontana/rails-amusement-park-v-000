58bb73d0b1ae0afa0ea8001c11103f22b84b4c43
laramontana <larabrightside@gmail.com>
2017-10-27 23:00:44 +0300

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 
+  before_action :set_user, only: [:show]
+
   def welcome
   end
 
@@ -8,6 +10,26 @@ class UsersController < ApplicationController
   end
 
   def create
-  end 
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :height, :happiness, :nausea, :tickets, :admin)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 
 end
